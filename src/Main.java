1ae1c35d69895cfa60e6c895c280cefdf38e1ba1
thisaramanohara <thisaramanohara@gmail.com>
2021-06-26 01:06:22 +0530

    Optimized version of bubble sort added

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main (String [] args) {
         int numberList [] = {2,4,3,45,6,34,23,43,54,34};
-        bubble_sort(numberList);
+        //bubble_sort_general(numberList);
+        //bubble_sort_optimized(numberList);
     }
 
-    static void bubble_sort(int [] data) {
-        //time complexity O(n^2)
+    static void bubble_sort_general(int [] data) {
         int temp;
         for(int i=0;i<data.length-1;i++) {
             for (int j=data.length-1;j>0;j--) {
@@ -22,4 +22,26 @@ public class Main {
             System.out.println(data[i]);
         }
     }
+
+    static void bubble_sort_optimized(int [] data) {
+        int temp;
+        boolean quit = false;
+        for(int i=0;i<data.length-1 && !quit;i++) {
+            quit = true;
+            for (int j=data.length-1;j>0;j--) {
+                if(data[j]<data[j-1]){
+                    //swap
+                    temp = data[j-1];
+                    data[j-1] = data[j];
+                    data[j] = temp;
+                    quit = false;
+                }
+            }
+        }
+        System.out.println();
+        for(int i=0;i<data.length;i++){
+            System.out.println(data[i]);
+        }
+    }
+
 }
